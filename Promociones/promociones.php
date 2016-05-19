07ba6d15db835674aaf2d1feb955ca4f5bb3bb25
Francisco López <jflvilchez94@hotmail.com>
2016-05-19 17:30:41 +0200

    Paso a Objetos de Actividades y Fotos Listado de actividades y promociones y la galeria de fotos todo desde la BBDD

--- Promociones/promociones.php
+++ Promociones/promociones.php
@@ -1,63 +1,97 @@
-
-    
-<div class="container">
-  	<div class="row">
+	<div class="container">
+	<div class="row">
   	   <div class="col m12 l12 offset-l3">
-		     <div id="info" style='margin-top: 20px;'>
-					<h3    style='padding-bottom: 20px;' class=' separador separadorDots'>Informacion de las promociones</h3>
+			<div id="info" style='margin-top: 20px;'>
+					<h3 style='padding-bottom: 20px;' class=' separador separadorDots'>Informacion de las promociones</h3>
 			</div>
-  		</div> 
-		<div class="col m12 l12 ">
-			<div class="row">
-				<div class="col m6 l6">
-				<img class='img-promo' src="./images/promociones/oferta10.jpg">
-				</div>
-				<div class="col m6 l6">
-					<div class='text-info-index-row'><h3>OFERTA 10% DE DESCUENTO</h3>
-					<p class="promo-text-info-index-row">Disfrute de un 10% de descuento con esta tarifa no reembolsable.</p>
-					<button class="btn waves-effect waves-light" type="submit" name="action">Ir a promocion
-					<i class="material-icons right">send</i>
-					</button>
-        
-					</div>
-					<div style="clear:both;"></div>
-				</div>
-			</div>
-		</div>
-		<div class="col m12 l12 ">
-			<div class="row white">
-				<div class="col l6">
-					<div class='text-info-index-row'><h3>OFERTA DOS NOCHES</h3>
-						<p class="promo-text-info-index-row ">Disfrute de un 10% de descuento en estancias de un mínimo de dos noches.</p>
-						<button class="btn waves-effect waves-light" type="submit" name="action">Ir a promocion
-						<i class="material-icons right">send</i>
-						</button>
-					</div>
-					<div style="clear:both;"></div>
-				</div>
-				<div class="col l6">
-					<img class='img-promo' align="right" src="./images/habitacion_triple.jpg">
-				</div>
-			</div>
-		</div>
-		<div class="col l12">
-			<div class="row">
-				<div class="col l6">
-				<img class='img-promo' src="./images/promociones/Flamenco2.jpg">
-				</div>
-				<div class="col l6">
-					<div class='text-info-index-row'><h3>HABITACIÓN DOBLE + ESPECTÁCULO DE FLAMENCO</h3>
-					<p class="promo-text-info-index-row">El Flamenco más puro nacido del corazón del monte Albayzín, vive un recuerdo de vida en la cueva flamenca más bonita de Andalucía de la que cuenta la leyenda que fue templo clandestino de cultos religiosos y donde ahora rendimos culto a nuestro arte.</p>
-					<button class="btn waves-effect waves-light" type="submit" name="action">Ir a promocion
-					<i class="material-icons right">send</i>
-					</button>
-        
-					</div>
-					<div style="clear:both;"></div>
-				</div>
-			</div>
-		</div>
+		</div> 
 
+	<?php 
+		//include './listaPromociones.php';
+		class Promocion {
 
+		   var $imagen;
+		   var $cabecera;
+		   var $descripcion;
+		   
+		  
+		   function Promocion($imagen,$cabecera,$descripcion) {
+		      $this->imagen = $imagen;
+		      $this->cabecera = $cabecera;
+		      $this->descripcion = $descripcion;
+		   }
+
+		   function printPromocionesLeft(){
+			   		echo '<div class="col m12 l12 ">
+							<div class="row">
+						   		<div class="col m6 l6">
+									<img class="img-promo" src="'.$this->getImagen().'">
+								</div>
+								<div class="col m6 l6">
+									<div class="text-info-index-row"><h3>'.$this->getCabecera().'</h3>
+									<p class="promo-text-info-index-row">'.$this->getDescripcion().'</p>
+									<button class="btn waves-effect waves-light" type="submit" name="action">Ir a promocion
+									<i class="material-icons right">send</i>
+									</button>
+	        
+								</div>
+								<div style="clear:both;"></div>
+							</div>
+						</div>
+					</div>';
+		   }
+
+		   function printPromocionesRight(){
+		   		echo '<div class="col m12 l12 ">
+						<div class="row white">
+							<div class="col l6">
+								<div class="text-info-index-row"><h3>'.$this->getCabecera().'</h3>
+									<p class="promo-text-info-index-row ">'.$this->getDescripcion().'</p>
+									<button class="btn waves-effect waves-light" type="submit" name="action">Ir a promocion
+									<i class="material-icons right">send</i>
+									</button>
+								</div>
+								<div style="clear:both;"></div>
+							</div>
+							<div class="col l6">
+								<img class="img-promo" align="right" src="'.$this->getImagen().'">
+							</div>
+						</div>
+					</div>';
+		   }
+
+		  function getImagen(){return $this->imagen;}
+		  function getCabecera(){return $this->cabecera;}
+		  function getDescripcion(){return $this->descripcion;}
+
+		  function setImagen($img){$imagen = $img;}
+		  function setCabecera($header){$cabecera = $header;}
+		  function setDescripcion($desc){$descripcion = $desc;}
+
+		  function listaPromos(){
+			  	include './conexion/conexion.php';
+				$mbd = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $usuario, $pass);
+				$sentencia ="SELECT nombre,precio,plazas_disponibles,descripcion_esp,imagen from `promocion`";
+				$totalPromos=0;
+
+				foreach($mbd->query($sentencia) as $row){
+					$promo = new Promocion($row['imagen'],$row['nombre'],$row['descripcion_esp']);
+					if($totalPromos%2==0){
+						$promo->printPromocionesLeft();
+					}else{
+						$promo->printPromocionesRight();
+					}
+					$totalPromos++;
+				}
+		  }
+
+		}
+
+		$p = new Promocion('','','');
+		$p->listaPromos();
+		
+	?>
+		</div>
 	</div>
-</div>
+    
+
